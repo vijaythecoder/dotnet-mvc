9decddd1d9d252e04affc6df6873ff6152547ab4
Denise Case <dcase@nwmissouri.edu>
2019-01-27 15:15:07 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# .NET MVC
+
+> Basic app from 'dotnet new mvc'.
+
+## Links
+
+[Source](https://github.com/profcase/dotnet-mvc)
+
+## New MVC
+
+Create a project folder.
+
+From inside the project folder, open PowerShell here as Administrator.
+
+Run:
+
+```PowerShell
+dotnet new mvc
+```
+
+Add additional files:
+
+- README.md
+- [.gitignore](https://github.com/github/gitignore/blob/master/VisualStudio.gitignore)
+
+## Run
+
+```PowerShell
+dotnet run
+```
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dotnet_mvc
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // Called by the runtime. Use to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<CookiePolicyOptions>(options =>
+      { // lambda determines whether user consent for non-essential cookies is needed for given request.
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      // In production, the Angular files will be served from this directory
+      services.AddSpaStaticFiles(configuration =>  { configuration.RootPath = "ClientApp/dist";  });
+    }
+
+    // Called by runtime. Use to configure HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();  // HTTP Strict Transport Security Protocol 
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseCookiePolicy();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
+}
